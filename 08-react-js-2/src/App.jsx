0b7903682d677de525b7ccfd4bd694e3611b6703
abukafa <hikam.abukafa@gmail.com>
2026-01-11 16:08:12 +0700

    add cart page

--- 08-react-js-2/src/App.jsx
+++ 08-react-js-2/src/App.jsx
@@ -1,10 +1,48 @@
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./routes/Home";
+import Cart from "./routes/Cart";
 
 function App() {
   return (
-    <div className="container m-4">
-      <Home />
-    </div>
+    <>
+      <nav className="navbar navbar-expand-lg bg-primary fixed-top mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            Lubna Store
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" to="/">
+                Beranda
+              </Link>
+              <Link className="nav-link" to="/cart">
+                Keranjang
+              </Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <h1>.</h1>
+
+      <div className="container mt-5">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
